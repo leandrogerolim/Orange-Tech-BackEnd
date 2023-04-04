d7375c6d6d5e956dd678ed06e672bbd9aa034595
Leandro <leandrogerolim@gmail.com>
2023-04-04 10:37:39 -0400

    Desafio concluído.

---
+++ Orange-Tech-BackEnd/src/DesafioMesadaDoSobrinho.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DesafioMesadaDoSobrinho {
+
+
+        public static void main(String[] args) {
+            Scanner leitor = new Scanner(System.in);
+            int entrada = leitor.nextInt();
+            int mesada = 50;
+
+
+
+            int valor = entrada * mesada;
+
+            System.out.println("Voce tera " +valor + " reais");
+
+        }
+    }
+
