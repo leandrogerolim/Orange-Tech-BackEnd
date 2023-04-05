033fda7193727ef3175753f7de612c9ea5dd4dcb
Leandro <leandrogerolim@gmail.com>
2023-04-04 21:03:00 -0400

    Desafio concluído.

---
+++ Orange-Tech-BackEnd/src/DesafioCamaroteDoBlueColdIceCubes.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DesafioCamaroteDoBlueColdIceCubes {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int tamanhoDaFila = leitor.nextInt();
+        int pessoasNoCamarote = 0;
+
+        for (int i = 0; i <= tamanhoDaFila; i++) {
+
+            if (i % 2 != 0) {
+
+                pessoasNoCamarote++;
+            }
+
+        }
+        System.out.println(pessoasNoCamarote + " pessoas no camarote");
+    }
+
+}
+
